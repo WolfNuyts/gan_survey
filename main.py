eff600ca886d5d68eb378197b97c2edf4d95e659
Wolf Nuyts <wolf.nuyts@gmail.com>
2021-04-21 14:45:57 +0200

    added reeksen

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ import numpy as np
 import random
 
 
-
-
 class Display(object):
-    def __init__(self):
+    def __init__(self, reeks):
         self.quality_q = 'Please rank the QUALITY from best(1) to worst(4):'
         self.cond_q = 'Please rank the RELEVANCE ACCORDING TO THE TEXT DESCRIPTION from best(1) to worst(4):'
+        self.reeks = reeks
         self.sample_size = 50
         self.i = 1
         self.results = np.zeros((self.sample_size*2, 4))
-        with open('data/captions.pkl', 'rb') as fh:
+        with open('data/' + self.reeks + '/captions.pkl', 'rb') as fh:
             self.captions = pickle.load(fh)
 
         self.root = Tk()
@@ -33,7 +32,7 @@ class Display(object):
         image_frame0.pack(side='left', pady=50, padx=20)
         canvas0 = Canvas(image_frame0, width=256, height=256)
         canvas0.pack()
-        self.img0 = PhotoImage(file="data/0/0.png")
+        self.img0 = PhotoImage(file="data/" + self.reeks + "/0/0.png")
         canvas0.create_image(0, 0, anchor='nw', image=self.img0)
         self.user_input0 = StringVar(self.root)
         entry0 = Entry(image_frame0, textvariable=self.user_input0)
@@ -43,7 +42,7 @@ class Display(object):
         image_frame1.pack(side=LEFT, pady=50, padx=20)
         canvas1 = Canvas(image_frame1, width=256, height=256)
         canvas1.pack()
-        self.img1 = PhotoImage(file="data/0/1.png")
+        self.img1 = PhotoImage(file="data/" + self.reeks + "/0/1.png")
         canvas1.create_image(0, 0, anchor='nw', image=self.img1)
         self.user_input1 = StringVar(self.root)
         entry1 = Entry(image_frame1, textvariable=self.user_input1)
@@ -53,7 +52,7 @@ class Display(object):
         image_frame2.pack(side='left')
         canvas2 = Canvas(image_frame2, width=256, height=256)
         canvas2.pack()
-        self.img2 = PhotoImage(file="data/0/2.png")
+        self.img2 = PhotoImage(file="data/" + self.reeks + "/0/2.png")
         canvas2.create_image(0, 0, anchor='nw', image=self.img2)
         self.user_input2 = StringVar(self.root)
         entry2 = Entry(image_frame2, textvariable=self.user_input2)
@@ -63,7 +62,7 @@ class Display(object):
         image_frame3.pack(side='left')
         canvas3 = Canvas(image_frame3, width=256, height=256)
         canvas3.pack()
-        self.img3 = PhotoImage(file="data/0/3.png")
+        self.img3 = PhotoImage(file="data/" + self.reeks + "/0/3.png")
         canvas3.create_image(0, 0, anchor='nw', image=self.img3)
         self.user_input3 = StringVar(self.root)
         entry3 = Entry(image_frame3, textvariable=self.user_input3)
@@ -100,7 +99,7 @@ class Display(object):
         if self.i == self.sample_size*2:
 
             self.results[self.i - 1] = np.array(inputs)
-            with open('results/results_' + str(random.randint(0, int(1e15))) + '.pkl', 'wb') as fh:
+            with open('results/results_' + self.reeks + '_' + str(random.randint(0, int(1e15))) + '.pkl', 'wb') as fh:
                 pickle.dump(self.results, fh)
             
             self.root.destroy()
@@ -130,7 +129,7 @@ class Display(object):
 
             self.question.configure(text=self.quality_q)
             self.text_descr.configure(text='')
-            image_folder = 'data/' + str(int(self.i/2)) + '/'
+            image_folder = 'data/' + self.reeks + '/' + str(int(self.i/2)) + '/'
             self.img0.configure(file=image_folder + '0.png')
             self.img1.configure(file=image_folder + '1.png')
             self.img2.configure(file=image_folder + '2.png')
@@ -140,25 +139,60 @@ class Display(object):
         return
 
 
+def start_reeks1(popup):
+    popup.destroy()
+    Display('reeks1')
+
+
+def start_reeks2(popup):
+    popup.destroy()
+    Display('reeks2')
+
+
+def start_reeks3(popup):
+    popup.destroy()
+    Display('reeks3')
+
+
+def start_reeks4(popup):
+    popup.destroy()
+    Display('reeks4')
+
+
+def start_reeks5(popup):
+    popup.destroy()
+    Display('reeks5')
+
+
 if __name__ == '__main__':
     popup = Tk()
     popup.wm_title("survey")
     greeting = Label(popup,
                      text="Hallo, "
                           "\n\nDeze survey onderzoekt hoe goed verschillende AI modellen realistische "
-                          "afbeeldingen kunnen onderzoeken. Hiervoor krijgt u 50 reeksen van 4 afbeeldingen te zien"
+                          "afbeeldingen kunnen genereren. Hiervoor krijgt u 50 reeksen van 4 afbeeldingen te zien. "
                           "Deze zal u 2 keer moeten ranken van beste(1) tot slechtste(4)."
                           "\n\nDe eerst keer vragen we u om deze afbeelding te ranken op de kwaliteit van de "
                           "afbeeldingen. Dit wil zeggen dat u ze moeten ranken op hoe realistisch ze eruit zien."
-                          "\n\nDe tweede keer vragen tonen we ook een tekst beschrijving van de afbeelding en "
+                          "\n\nDe tweede keer tonen we ook een tekst beschrijving van de afbeelding en "
                           "vragen we u om de afbeelding te ranken op hoe goed deze afbeeldingen afbeelden wat er in "
-                          "de tekst beschrijving straat. De bedoeling is om deze keer de kwaliteit helemaal NIET in "
-                          "rekening te brengen."
+                          "de tekst beschreven straat. De bedoeling is om deze keer de kwaliteit helemaal NIET in "
+                          "rekening te brengen maar dus enkel te focussen dat alle delen van de tekst beschrijving "
+                          "afgebeeld zijn."
+                          "\n\n U kan de survey starten door op de onderstaande startknop te drukken van de reeks "
+                          "die aan u werd toegewezen."
                           "\n\nalvast bedankt voor u medewerking,"
                           "\nWolf",
                      wraplengt=500, anchor='w', justify='left')
     greeting.pack()
-    startBut = Button(popup, text="Start", fg="black", command=popup.destroy)
-    startBut.pack()
+    reeks1But = Button(popup, text="start reeks1", fg="black", command=lambda: start_reeks1(popup))
+    reeks1But.pack(side='left', pady=10, padx=10)
+    reeks2But = Button(popup, text="start reeks2", fg="black", command=lambda: start_reeks2(popup))
+    reeks2But.pack(side='left', pady=10, padx=10)
+    reeks3But = Button(popup, text="start reeks3", fg="black", command=lambda: start_reeks3(popup))
+    reeks3But.pack(side='left', pady=10, padx=10)
+    reeks4But = Button(popup, text="start reeks4", fg="black", command=lambda: start_reeks4(popup))
+    reeks4But.pack(side='left', pady=10, padx=10)
+    reeks5But = Button(popup, text="start reeks5", fg="black", command=lambda: start_reeks5(popup))
+    reeks5But.pack(side='left', pady=10, padx=10)
     popup.mainloop()
-    Display()
